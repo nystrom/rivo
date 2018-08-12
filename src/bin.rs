6106e247df9387abcca1447ce94d3dd8cb4335ef
Nate Nystrom <nate.nystrom@usi.ch>
2018-08-12 17:37:59 +0200

    main tests that visitors work, at least a little bit.

--- src/bin.rs
+++ src/bin.rs
@@ -1,7 +1,15 @@
 extern crate ivo;
+
 use ivo::parser::parse_string;
+use ivo::syntax::visit::Rewriter;
+
+struct SmokeTest;
+impl<'a> Rewriter<'a> for SmokeTest {}
 
 fn main() {
     let t = parse_string(String::from("1+2"));
     println!("{:?}", *t);
+
+    let s = SmokeTest.visit_root(&*t);
+    println!("{:?}", s);
 }
