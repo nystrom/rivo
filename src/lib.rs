6765e3bd9e7b04c32e0dd9503bcef5a01496df91
Nate Nystrom <nate.nystrom@usi.ch>
2018-08-14 21:21:55 +0200

    add unicode-categories for the lexer. turn off some warnings.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
 extern crate ansi_term;
 extern crate term;
 // #[macro_use] extern crate lazy_static;
@@ -5,6 +9,7 @@ extern crate term;
 // #[cfg(test)] #[macro_use] extern crate pretty_assertions;
 extern crate rustc_serialize;
 extern crate num;
+extern crate unicode_categories;
 
 pub mod parser;
 pub mod syntax;
