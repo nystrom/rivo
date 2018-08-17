89fe60da094a7db1024c317db359fa1aa1673dfc
Nate Nystrom <nate.nystrom@usi.ch>
2018-08-18 00:50:04 +0200

    Added more factory methods.

--- src/syntax/loc.rs
+++ src/syntax/loc.rs
@@ -1,4 +1,5 @@
 use std::ops::{Deref, DerefMut};
+use std::fmt;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Loc {
@@ -17,15 +18,70 @@ pub struct Pos {
 pub const NO_POS: Pos = Pos { offset: 0, line: 1, column: 1 };
 pub const NO_LOC: Loc = Loc { start: NO_POS, end: NO_POS, source: None };
 
+impl Loc {
+    pub fn no_loc() -> Loc { NO_LOC }
+
+    pub fn prelude_loc() -> Loc {
+        Loc {
+            start: NO_POS,
+            end: NO_POS,
+            source: Some(String::from("Prelude.ivo"))
+        }
+    }
+
+    pub fn span(start: Loc, end: Loc) -> Loc {
+        Loc {
+            start: start.start,
+            end: end.end,
+            source: start.source.clone()
+        }
+    }
+
+    pub fn span_from<T, U>(start: Located<T>, end: Located<U>) -> Loc {
+        Loc {
+            start: start.loc.start,
+            end: end.loc.end,
+            source: start.loc.source.clone()
+        }
+    }
+}
+
 #[derive(Clone, Debug, PartialEq)]
 pub struct Located<T> {
     pub loc: Loc,
     pub value: T,
 }
 
+impl fmt::Display for Loc {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.source {
+            Some(ref src) =>
+                write!(f, "{}:{}-{}", src, self.start, self.end),
+            None =>
+                write!(f, "(input):{}-{}", self.start, self.end),
+        }
+    }
+}
+
+impl fmt::Display for Pos {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}:{}", self.line, self.column)
+    }
+}
+
 impl<T> Located<T> {
-    pub fn new(t: T, start: Pos, end: Pos, source: String) -> Located<T> {
-        Located { value: t, loc: Loc { start: start, end: end, source: Some(source) } }
+    pub fn new(loc: Loc, value: T) -> Located<T> {
+        Located {
+            loc,
+            value
+        }
+    }
+
+    pub fn new_from(other: Located<T>, value: T) -> Located<T> {
+        Located {
+            loc: other.loc.clone(),
+            value
+        }
     }
 
     pub fn with_loc(self, loc: &Loc) -> Located<T> {
@@ -42,6 +98,23 @@ impl<T> Located<T> {
     pub fn map_with_loc<U, F>(self, f: F) -> Located<U> where F: Fn(Loc, T) -> U {
         Located { loc: self.loc.clone(), value: f(self.loc.clone(), self.value) }
     }
+}
+
+pub fn loc_of<T>(locs: Vec<Located<T>>) -> Loc {
+    let mut loc = NO_LOC;
+    for e in locs {
+        if loc == NO_LOC {
+            loc = e.loc
+        }
+        else {
+            loc = Loc {
+                start: loc.start,
+                end: e.loc.end,
+                source: loc.source
+            }
+        }
+    }
+    loc
 }
 
 /// Can get a reference to the tree by dereferencing (`*located`)
