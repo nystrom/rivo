1bfcd29bdef6da34aeab06abcc9eda3e6ea99045
Nate Nystrom <nate.nystrom@usi.ch>
2018-08-31 11:10:34 +0200

    utility methods for names.

--- src/syntax/names.rs
+++ src/syntax/names.rs
@@ -11,3 +11,87 @@ pub enum Part {
     Op(String),
     Placeholder,
 }
+
+impl Part {
+    pub fn to_name(&self) -> Name {
+        match *self {
+            Part::Id(ref x) => Name::Id(x.clone()),
+            Part::Op(ref x) => Name::Op(x.clone()),
+            Part::Placeholder => Name::Op("_".to_string())
+        }
+    }
+
+    pub fn is_id(&self) -> bool {
+        match *self {
+            Part::Id(_) => true,
+            _ => false,
+        }
+    }
+    pub fn is_op(&self) -> bool {
+        match *self {
+            Part::Op(_) => true,
+            _ => false,
+        }
+    }
+    pub fn is_placeholder(&self) -> bool {
+        match *self {
+            Part::Placeholder => true,
+            _ => false,
+        }
+    }
+
+}
+
+impl Name {
+    pub fn is_bundle_name(&self) -> bool {
+        match *self {
+            Name::Id(_) => true,
+            Name::Op(_) => false,
+            Name::Mixfix(ref parts) => {
+                self.is_prefix_name() && parts.first().unwrap().to_name().is_bundle_name()
+            },
+        }
+    }
+
+    pub fn is_prefix_name(&self) -> bool {
+        match *self {
+            Name::Id(_) => false,
+            Name::Op(_) => false,
+            Name::Mixfix(ref parts) => {
+                ! parts.is_empty() &&
+                parts.first().unwrap().is_id() &&
+                parts[1..].iter().all(|p| p.is_placeholder())
+            },
+        }
+    }
+    pub fn is_brackets_name(&self) -> bool {
+        match *self {
+            Name::Id(_) => false,
+            Name::Op(_) => false,
+            Name::Mixfix(ref parts) => {
+                ! parts.is_empty() &&
+                ! parts.first().unwrap().is_placeholder() &&
+                ! parts.last().unwrap().is_placeholder()
+            },
+        }
+    }
+
+    pub fn is_id(&self) -> bool {
+        match *self {
+            Name::Id(_) => true,
+            _ => false,
+        }
+    }
+    pub fn is_op(&self) -> bool {
+        match *self {
+            Name::Op(_) => true,
+            _ => false,
+        }
+    }
+    pub fn is_mixfix(&self) -> bool {
+        match *self {
+            Name::Mixfix(_) => true,
+            _ => false,
+        }
+    }
+}
