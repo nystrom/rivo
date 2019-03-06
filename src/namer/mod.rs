5f2e5b98bacc707c87db7f21986a4085a8516727
Nate Nystrom <nate.nystrom@gmail.com>
2019-03-06 22:07:15 +0100

    removed dead code

--- src/namer/mod.rs
+++ src/namer/mod.rs
@@ -8,31 +8,3 @@ mod earley;
 mod worklist;
 
 // mod cheap_renamer;
-
-// mod play {
-//     use datafrog::Iteration;
-
-//     fn foo() {
-//         // Create a new iteration context, ...
-//         let mut iteration = Iteration::new();
-
-//         // .. some variables, ..
-//         let nodes_var = iteration.variable::<(u32, u32)>("nodes");
-//         let edges_var = iteration.variable::<(u32, u32)>("edges");
-
-//         let nodes = vec![(1, 2), (3, 4), (5, 6)];
-//         let edges = vec![(1, 2), (2, 6)];
-//         // .. load them with some initial values, ..
-//         nodes_var.insert(nodes.into());
-//         edges_var.insert(edges.into());
-
-//         // .. and then start iterating rules!
-//         while iteration.changed() {
-//             // nodes(a,c)  <-  nodes(a,b), edges(b,c)
-//             nodes_var.from_join(&nodes_var, &edges_var, |_b, &a, &c| (c, a));
-//         }
-
-//         // extract the final results.
-//         let reachable: Vec<(u32, u32)> = nodes_var.complete();
-//     }
-// }
